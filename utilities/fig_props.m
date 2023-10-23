dd7f9348190033cfafdb308363d51808c0199d75
Eva Takou <etakou@vt.edu>
2023-10-23 10:33:20 -0400

    updates

--- utilities/fig_props.m
+++ utilities/fig_props.m
@@ -1,5 +1,15 @@
 function fig_props(fntsize,xlab,ylab)
-
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: Oct 23, 2023
+%--------------------------------------------------------------------------
+%
+%Function to set default properties for plots.
+%
+%Input: fntsize: Size of font in figure
+%       xlab: xlabel
+%       ylab: ylabel
+%--------------------------------------------------------------------------
 
 set(gca,'fontname','microsoft sans serif','fontsize',fntsize)
 set(gcf,'color','w')
