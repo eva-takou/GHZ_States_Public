7272e59d740f1d3bb0cc6477993398b6c795a2c6
Eva Takou <etakou@vt.edu>
2023-10-23 09:24:03 -0400

    updated comments

--- Simulations/Tolerances_Multispin.m
+++ Simulations/Tolerances_Multispin.m
@@ -1,15 +1,27 @@
 function [Target_One_Tangle_Tol,Unwanted_One_Tangle_Tol,Infid_Tol,Time_Max,MaxRes,Time_Vals]=...
                      Tolerances_Multispin(GHZsize)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: Oct 23, 2023
+%--------------------------------------------------------------------------
+%
+%Script to set the tolerances for the optimization of the multispin 
+%entangling scheme optimization to reproduce the results
+%of Ref. https://arxiv.org/abs/2302.05580.
+%--------------------------------------------------------------------------
+%
+%Input: GHZsize>=3, which is the size of the GHZ state.
+%--------------------------------------------------------------------------
 
-MaxRes=10; 
+MaxRes=10; %Max resonance index
 
 if GHZsize==3
     
-    Time_Max                = 2000;
-    Target_One_Tangle_Tol   = 0.9;
-    Unwanted_One_Tangle_Tol = 0.1;
-    Infid_Tol               = 0.11;
-    Time_Vals               = 500;  
+    Time_Max                = 2000; %Total time of sequence (\mu s)
+    Target_One_Tangle_Tol   = 0.9;  %Tolerance of target one-tangles
+    Unwanted_One_Tangle_Tol = 0.1;  %Tolerance of unwanted one-tangles 
+    Infid_Tol               = 0.11; %Tolerance for infidelity of multispin gate
+    Time_Vals               = 500;  %# of time values to sample (determines time-step)
     
 elseif GHZsize==4
     
--- Simulations/Tolerances_Sequential.m
+++ Simulations/Tolerances_Sequential.m
@@ -1,15 +1,27 @@
 function [Target_One_Tangle_Tol,Unwanted_One_Tangle_Tol,Infid_Tol,Time_Max,Individual_Time_Max,MaxRes]=...
                      Tolerances_Sequential(GHZsize)
+%--------------------------------------------------------------------------
+%Created by Eva Takou
+%Last modified: Oct 23, 2023
+%--------------------------------------------------------------------------
+%
+%Script to set the tolerances for the optimization of the sequential 
+%entangling scheme optimization to reproduce the results
+%of Ref. https://arxiv.org/abs/2302.05580.
+%--------------------------------------------------------------------------
+%
+%Input: GHZsize>=3, which is the size of the GHZ state.
+%--------------------------------------------------------------------------
 
-MaxRes=18; 
+MaxRes=18;  %Max resonance index
 
 if GHZsize==3
     
-    Time_Max                = 2000; 
-    Individual_Time_Max     = 2000;
-    Unwanted_One_Tangle_Tol = 0.1;
-    Target_One_Tangle_Tol   = 0.99;
-    Infid_Tol               = 0.1;
+    Time_Max                = 2000; %Total time of composite sequence (\mu s)
+    Individual_Time_Max     = 2000; %Time of individual sequence (\mu s)
+    Unwanted_One_Tangle_Tol = 0.1;  %Tolerance of unwanted one-tangles 
+    Target_One_Tangle_Tol   = 0.99; %Tolerance of target one-tangles
+    Infid_Tol               = 0.1;  %Tolerance for infidelity of composite gate
     
 elseif GHZsize==4
     
