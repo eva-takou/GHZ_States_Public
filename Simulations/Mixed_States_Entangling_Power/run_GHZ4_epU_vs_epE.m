10c8a87e8872881ee4ca08bd039ee3e00dbe5797
Eva Takou <etakou@vt.edu>
2023-10-28 22:11:27 -0400

    updates

--- Simulations/Mixed_States_Entangling_Power/run_GHZ4_epU_vs_epE.m
+++ Simulations/Mixed_States_Entangling_Power/run_GHZ4_epU_vs_epE.m
@@ -1,10 +1,10 @@
 %--------------------------------------------------------------------------
 %Created by Eva Takou
-%Last modified: Oct 23, 2023
+%Last modified: Oct 28, 2023
 %--------------------------------------------------------------------------
 
 clc
-path_Sequential='/Users/evatakou/Documents/MATLAB/Defect_Nuclear_GHZ_states/GHZ_states_Preparation/Simulations/GHZ_Data_of_Sequential_NEW/GHZ4_Sequential.mat';
-path_Multispin ='/Users/evatakou/Documents/MATLAB/Defect_Nuclear_GHZ_states/GHZ_states_Preparation/Simulations/GHZ_Data_of_MultiSpin_NEW/GHZ4_Multispin.mat';
+path_Sequential='/Users/evatakou/Documents/MATLAB/GHZ_States_Public/Simulations/GHZ_Data_of_Sequential_FINAL/GHZ4_Sequential.mat';
+path_Multispin ='/Users/evatakou/Documents/MATLAB/GHZ_States_Public/Simulations/GHZ_Data_of_MultiSpin_NEW/GHZ4_Multispin.mat';
 %unitary_vs_non_unitary_MwayEP(path_Sequential,path_Multispin)
 unitary_vs_non_unitary_MwayEP_Analytical(path_Sequential,path_Multispin)
